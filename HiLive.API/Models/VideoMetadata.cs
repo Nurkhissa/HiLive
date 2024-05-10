c5134a487577f09c7c621e608606ef6ac18fa665
kuralov29 <kuralov29@outlook.com>
2024-05-10 20:56:52 +0500

    Models added

--- HiLive.API/Models/VideoMetadata.cs
+++ HiLive.API/Models/VideoMetadata.cs
@@ -2,6 +2,13 @@
 {
     public class VideoMetadata
     {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string BlobPath { get; set; }
+        public string Thubnail { get; set; }
+        public DateTimeOffset CreatedDate { get;} 
+        public DateTimeOffset UpdatedDate { get;}
 
     }
 }
