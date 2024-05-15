4a685514b1860743fc9afdf72d4099cf2a5cf457
tohirjon-odilov <shahanshoh819@gmail.com>
2024-05-15 22:03:32 +0500

    CODE RUB: Add Header and fixed seed data

--- HiLive.API/Brokers/Storoges/IStorageBroker.cs
+++ HiLive.API/Brokers/Storoges/IStorageBroker.cs
@@ -1,4 +1,9 @@
-﻿using HiLive.API.Models.VideoMetadatas;
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using HiLive.API.Models.VideoMetadatas;
 
 namespace HiLive.API.Brokers.Storoges
 {
