cc851313d8a4deb25cd4d1fab1ea5e4224b9e4b0
tohirjon-odilov <shahanshoh819@gmail.com>
2024-05-15 19:22:43 +0500

    SERVICE: Add VideoMetadatas service for post and get

---
+++ HiLive.API/Services/VideoMetadatas/VideoMetadatasService.cs
@@ -0,0 +1,36 @@
+﻿using HiLive.API.Brokers.Storoges;
+using HiLive.API.Models.VideoMetadatas;
+
+namespace HiLive.API.Services.VideoMetadatas
+{
+    public class VideoMetadatasService : IVideoMetadatasService
+    {
+
+        private readonly IStorageBroker storageBroker;
+
+        public VideoMetadatasService(IStorageBroker storageBroker)
+        {
+            this.storageBroker = storageBroker;
+        }
+
+        public async ValueTask<VideoMetadata> AddVideoMetadataAsync(VideoMetadata category) => 
+            await this.storageBroker.InsertVideoMetadataAsync(category);
+
+        public ValueTask<VideoMetadata> ModifyVideoMetadataAsync(VideoMetadata category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async ValueTask<VideoMetadata?> RemoveVideoMetadatasByIdAsync(Guid categoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<VideoMetadata> RetrieveAllVideoMetadatas() => this.storageBroker.SelectAllVideoMetadatas();
+
+        public async ValueTask<VideoMetadata?> RetrieveVideoMetadataByIdAsync(Guid categoryId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
