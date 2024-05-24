33288d76d78d86af3a408e8f564370ad6674eee5
kuralov29 <kuralov29@outlook.com>
2024-05-24 22:04:57 +0500

    BROKERS: Implement Logging

--- HiLive.API/Brokers/Loggings/ILoggingBroker.cs
+++ HiLive.API/Brokers/Loggings/ILoggingBroker.cs
@@ -3,13 +3,17 @@
 //// FREE TO USE TO CONNECT THE WORLD
 //// ---------------------------------------------------------------
 
-//using System;
+using System;
 
-//namespace HiLive.API.Brokers.Loggings
-//{
-//    public interface ILoggingBroker
-//    {
-//        void LogCriticalError(Exception exception);
-//        void LogError(Exception exception); 
-//    }
-//}
+namespace HiLive.API.Brokers.Loggins
+{
+    public interface ILoggingBroker
+    {
+        void LogInformation(string message);
+        void LogTrace(string message);
+        void LogDebug(string message);
+        void LogWarning(string message);
+        void LogError(Exception exception);
+        void LogCritical(Exception exception);
+    }
+}
---
+++ HiLive.API/Brokers/Loggings/LoggingBroker.cs
@@ -0,0 +1,34 @@
+﻿using HiLive.API.Brokers.Loggins;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace HiLive.API.Brokers.Loggings
+{
+    public class LoggingBroker : ILoggingBroker
+    {
+        private readonly ILogger<LoggingBroker> logger;
+        public void LogCritical(Exception exception) =>
+            this.logger.LogCritical(exception, exception.Message);
+
+
+        public void LogDebug(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogError(Exception exception) =>
+            this.logger.LogError(exception, exception.Message);
+
+
+        public void LogInformation(string message) =>
+            this.logger.LogInformation(message);
+
+
+        public void LogTrace(string message) =>
+            this.logger.LogTrace(message);
+
+
+        public void LogWarning(string message) =>
+            this.logger.LogWarning(message);
+    }
+}
